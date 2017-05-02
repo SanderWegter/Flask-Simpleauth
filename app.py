356519a7dfc823cd2e08791a4bf8c3c8b41d1731
Sander Wegter <sander@puur-it.nl>
2017-05-02 15:50:17 +0200

    fixed keyError

--- app.py
+++ app.py
@@ -139,7 +139,10 @@ def register():
 		else:
 			message = {'message': 'Something went wrong: '+result, 'type': 'error'}
 			return render_template('register.html', message=message)
-	if config['registration_enabled'] or session['username'] == 'admin':
+	if 'username' in session:
+		if session['username'] == 'admin':
+			return render_template('register.html', message=message)
+	if config['registration_enabled']:
 		return render_template('register.html', message=message)
 	else:
 		notifications = {'message': 'User registration is disabled by the admin', 'type': 'warning'}
